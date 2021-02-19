6dc00a501bd9102a4556e31eaef0675ced0f7766
Luke Rollins <ldrollins@gmail.com>
2021-02-19 14:06:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,9 @@
 def turn_count(board)
+
   counter = 0
-board.each do |turn|
-  if turn == "X" || "O"
-    counter =+ 1
+  board.each do |turn|
+    if turn == "X" || "O"
+      counter += 1
+    end
+  end
+end
